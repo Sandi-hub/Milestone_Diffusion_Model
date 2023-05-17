26ab1a5134b3ba3bf03a8d5c42b0c103e533c588
Sandra Rudeloff <srudeloff@outlook.com>
2023-05-17 15:44:19 -0700

    formatting

--- hyman_init.py
+++ hyman_init.py
@@ -1,13 +1,18 @@
 import os
 
+import geopandas as gpd
+
 from gravity_model import *
 
 """ 
 Here, the input parameter for the Hyman algorithm can be set
 """
 
-empirical_mean_shopping_distance = 4.65  # taken from Schlaich(2020); all units are in km
+empirical_mean_shopping_distance = (
+    4.65  # taken from Schlaich(2020); all units are in km
+)
 tolerance = 0.01
+
 
 def import_population_data():
     gdf_population = gpd.read_file(
@@ -22,11 +27,12 @@ def import_population_data():
 
     # The value of -1 in the column "population" means either uninhabited or to be kept secret. We will assume that all cells with a value of -1 are uninhabited
     df_population["population"] = df_population["population"].replace([-1], 0)
-    
+
     os.makedirs("Outputs/Population", exist_ok=True)
     df_population.to_pickle("Outputs/Population/population.pkl")
-    
+
     return df_population
+
 
 def import_shop_data():
     gdf_Hamburg_shops = gpd.read_file(
@@ -38,19 +44,22 @@ def import_shop_data():
     df_Hamburg_shops = df_Hamburg_shops.rename(
         columns={"Einwohner": "population", "Gitter_ID_": "Gitter_ID"}
     )
-    
+
     os.makedirs("Outputs/Retailer", exist_ok=True)
     df_Hamburg_shops.to_pickle("Outputs/Retailer/stores.pkl")
-    
+
     return df_Hamburg_shops
+
 
 df_population = import_population_data()
 df_Hamburg_shops = import_shop_data()
 
-flow = hyman_model(empirical_mean_shopping_distance, tolerance, df_population, df_Hamburg_shops)
+flow = hyman_model(
+    empirical_mean_shopping_distance, tolerance, df_population, df_Hamburg_shops
+)
 
 os.makedirs("Outputs/Flow", exist_ok=True)
 flow.to_pickle("Outputs/Flow/flow.pkl")
-#flow.to_csv("Outputs/Flow/flow.csv")
+# flow.to_csv("Outputs/Flow/flow.csv")
 
 print(flow)
